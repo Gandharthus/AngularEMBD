d1ddc0e0a89014c0eb997325f0019ea60c618f61
Gandharthus <72447580+Gandharthus@users.noreply.github.com>
2023-03-10 10:56:43 +0100

    h

---
+++ emdb/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment= {
+  firebase: {
+    projectId: 'emdb-85f79',
+    appId: '1:819617799322:web:9f8961dd156efb2c63d323',
+    storageBucket: 'emdb-85f79.appspot.com',
+    apiKey: 'AIzaSyCGzeDVndH8vQYy2YlUQYb3eWXrE7v5_tU',
+    authDomain: 'emdb-85f79.firebaseapp.com',
+    messagingSenderId: '819617799322',
+  },
+    production: false
+};
